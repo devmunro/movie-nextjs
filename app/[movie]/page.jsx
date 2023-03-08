218086ee7e308d6e345e6194a3707a5c6a474b6d
David Munro <defmunro@gmail.com>
2023-03-08 10:51:27 +0000

    added styles and genre

--- app/[movie]/page.jsx
+++ app/[movie]/page.jsx
@@ -1,38 +1,65 @@
 import Image from "next/image";
 
-
 export default async function movie({ params }) {
-   const {movie} = params
-   const api = `https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.API_KEY}`
-console.log(params)
+  const { movie } = params;
+  const api = `https://api.themoviedb.org/3/movie/${movie}?api_key=${process.env.API_KEY}`;
+  console.log(params);
   const imagePath = "https://image.tmdb.org/t/p/original";
-  const data = await fetch(api)
-  console.log(api)
+  const data = await fetch(api);
+  console.log(api);
   const response = await data.json();
+
+    // define an array of colors
+    const colors = [
+      "bg-red-500",
+      "bg-blue-500",
+      "bg-slate-500",
+      "bg-purple-500",
+    ];
 
   return (
     <div>
-    <div className="">
-      <h2 className="text-4xl">{response.title}</h2>
-      <h1 className="text-lg ">{response.release_date}</h1>
-      <h2>Runtime: {response.runtime} minutes</h2>
-      <h2 className="bg-green-600 inline-block my-2 py-2 px-4 rounded-lg text-sm">
-        {response.status}
-      </h2>
-      <div className="flex flex-wrap">
-      <Image
-        className="my-12 w-full"
-        src={imagePath + response.backdrop_path}
-        alt={response.title}
-        width={1000}
-        height={1000}
-        priority
-      />
+      <div className="">
+        <h1 className="text-4xl">{response.title}</h1>
+        <h2 className="text-lg">{response.release_date}</h2>
+        <h3>Runtime: {response.runtime} minutes</h3>
+        <div className="md:flex gap-8 my-4">
+          <h2 className="bg-green-600 inline-block my-2 py-2 px-4 rounded-lg md:text-sm">
+            {response.status}
+          </h2>{" "}
+          <div className="w-full">
+            <ul className="flex gap-2 [&>*]:bg-blue-600 [&>*]:my-2 [&>*]:py-2 [&>*]:px-4 [&>*]:rounded-lg md:text-sm">
+              {response.genres.map((genre) => (
+                <li key={genre.id}>{genre.name}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+
+        <div className="lg:flex w-full my-12 lg:gap-4">
+          <Image
+            className=" w-full lg:w-1/2"
+            src={imagePath + response.backdrop_path}
+            alt={response.title}
+            width={1000}
+            height={1000}
+            priority
+          />
+
+          <div className="my-4 w-full lg:w-1/4 lg:text-lg">
+            <p className="text-md">{response.overview}</p>
+          </div>
+        </div>
+
+        <div className="my-4 w-full">
+          <h3>Production Companies:</h3>
+          <ul>
+            {response.production_companies.map((company) => (
+              <li key={company.id}>{company.name}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
-    <div className="my-4 w-full">
-      <p className="text-md">{response.overview}</p>
-    </div>
-    </div>
-  </div>
   );
 }
